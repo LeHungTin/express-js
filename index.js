9b2eae76a68bdbf73177abbe4fbd2a4b10031769
tin1806 <lehungtin.ktqs@gmail.com>
2019-05-07 16:46:10 +0700

    lession 8 - Epress Router

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 var express = require('express');
-var app = express();
-var port = 3000;
 var bodyParser = require('body-parser');
-var shortId = require('shortid');
 
-var low = require('lowdb')
-var FileSync = require('lowdb/adapters/FileSync')
-var adapter = new FileSync('db.json')
-var db = low(adapter)
+var userRoute = require('./routes/user.route');
 
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ users: [] })
-  .write()
+var port = 3000;
 
-
-// app.get('/', function(request, response) {
-// 	response.send('Hello World');
-// });
-
-// app.get('/users', function(request, response) {
-// 	response.send('users list');
-// });
+var app = express();
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -32,44 +17,7 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.get('/users', function (req, res) {
-  res.render('users/index', {
-  	users: db.get('users').value()
-  });
-});
-
-app.get('/users/search', function (req, res) {
-	var q = req.query.q;
-	var matchedUsers = db.get('users').value().filter(function(user) {
-  		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  	});
-
-  res.render('users/index', {
-  	users: matchedUsers
-  });
-});
-
-app.get('/users/create', function (req, res) {
-  res.render('users/create');
-});
-
-app.get('/users/:id', function(req, res) {
-  var id = req.params.id;
-
-  var user = db.get('users').find({ id: id }).value();
-
-  res.render('users/view', {
-    user: user
-  });
-
-});
-
-app.post('/users/create', function (req, res) {
-  req.body.id = shortId.generate();
- 	db.get('users').push(req.body).write();
- 	res.redirect('/users');
-});
-
+app.use('/users', userRoute);
 
 app.listen(port, function() {
 	console.log('Express listen on port ' + port);
