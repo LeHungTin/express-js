07e0731da3afad82fbdab4dd327089f5a268f959
tin1806 <lehungtin.ktqs@gmail.com>
2019-05-07 16:15:13 +0700

    lession 7 - view user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = 3000;
 var bodyParser = require('body-parser');
+var shortId = require('shortid');
 
 var low = require('lowdb')
 var FileSync = require('lowdb/adapters/FileSync')
@@ -39,12 +40,12 @@ app.get('/users', function (req, res) {
 
 app.get('/users/search', function (req, res) {
 	var q = req.query.q;
-	var matchedUsers = db.get('users').filter(function(user) {
+	var matchedUsers = db.get('users').value().filter(function(user) {
   		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
   	});
 
   res.render('users/index', {
-  	users: matchedUsers.value()
+  	users: matchedUsers
   });
 });
 
@@ -52,10 +53,23 @@ app.get('/users/create', function (req, res) {
   res.render('users/create');
 });
 
+app.get('/users/:id', function(req, res) {
+  var id = req.params.id;
+
+  var user = db.get('users').find({ id: id }).value();
+
+  res.render('users/view', {
+    user: user
+  });
+
+});
+
 app.post('/users/create', function (req, res) {
+  req.body.id = shortId.generate();
  	db.get('users').push(req.body).write();
  	res.redirect('/users');
 });
+
 
 app.listen(port, function() {
 	console.log('Express listen on port ' + port);
