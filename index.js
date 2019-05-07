2e9c3aca08013e739bf91f9fbd2905efed7611ee
tin1806 <lehungtin.ktqs@gmail.com>
2019-05-07 10:48:35 +0700

    lession 2 - template engines

--- index.js
+++ index.js
@@ -2,8 +2,28 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-app.get('/', function(request, response) {
-	response.send('Hello World');
+// app.get('/', function(request, response) {
+// 	response.send('Hello World');
+// });
+
+// app.get('/users', function(request, response) {
+// 	response.send('users list');
+// });
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/users', function (req, res) {
+  res.render('users/index', {
+  	users: [
+  		{id: 1, name: 'Tin'},
+  		{id: 2, name: 'Quang'}
+  	]
+  });
 });
 
 app.listen(port, function() {
