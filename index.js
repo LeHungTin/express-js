9ee827a2b0bdce9201cf1515f4d1b156395f500f
tin1806 <lehungtin.ktqs@gmail.com>
2019-05-09 15:09:48 +0700

    lession 14 - cookies

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var userRoute = require('./routes/user.route');
 var productRoute = require('./routes/product.route');
@@ -13,6 +14,7 @@ app.set('view engine', 'pug');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
 
 app.use(express.static('public'));
 
