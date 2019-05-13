1fa947e2b2f70db203e23671bf693168229edca8
tin1806 <lehungtin.ktqs@gmail.com>
2019-05-13 13:57:30 +0700

    lession 20 - Pagination

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-console.log(process.env.SESSION_SECRETE);
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
