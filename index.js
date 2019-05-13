29fda0b67c2c52459f604ad8015edeb6aa905ec7
tin1806 <lehungtin.ktqs@gmail.com>
2019-05-13 16:22:52 +0700

    lession 22 - Session

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ var cookieParser = require('cookie-parser');
 var userRoute = require('./routes/user.route');
 var productRoute = require('./routes/product.route');
 var authRoute = require('./routes/auth.route');
+var cartRoute = require('./routes/cart.route');
 
 var authMiddleware = require('./middlewares/auth.middleware');
+var sessionMiddleware = require('./middlewares/session.middleware');
 
 var port = 3000;
 
@@ -20,6 +22,7 @@ app.set('view engine', 'pug');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cookieParser(process.env.SESSION_SECRETE));
+app.use(sessionMiddleware); // co tac dong den tat ca cac duong dan ma chung ta su dung
 
 app.use(express.static('public'));
 
@@ -30,6 +33,7 @@ app.get('/', function (req, res) {
 app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/products', productRoute);
 app.use('/auth', authRoute);
+app.use('/cart', cartRoute);
 
 app.listen(port, function() {
 	console.log('Express listen on port ' + port);
