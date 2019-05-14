d461d77eb0b801be376a217a34799fe44b9a69f7
tin1806 <lehungtin.ktqs@gmail.com>
2019-05-14 09:44:02 +0700

    lession 23 - CSRF attack

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var csurf = require('csurf');
 
 var userRoute = require('./routes/user.route');
 var productRoute = require('./routes/product.route');
 var authRoute = require('./routes/auth.route');
 var cartRoute = require('./routes/cart.route');
+var transferRoute = require('./routes/transfer.route');
 
 var authMiddleware = require('./middlewares/auth.middleware');
 var sessionMiddleware = require('./middlewares/session.middleware');
@@ -23,6 +25,7 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cookieParser(process.env.SESSION_SECRETE));
 app.use(sessionMiddleware); // co tac dong den tat ca cac duong dan ma chung ta su dung
+app.use(csurf({ cookie: true })); // la 1 ham tra ve 1 middleware
 
 app.use(express.static('public'));
 
@@ -34,6 +37,7 @@ app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/products', productRoute);
 app.use('/auth', authRoute);
 app.use('/cart', cartRoute);
+app.use('/transfer', authMiddleware.requireAuth, transferRoute);
 
 app.listen(port, function() {
 	console.log('Express listen on port ' + port);
